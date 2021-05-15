01384d86aacdbbf7856a104c0665a44098e76d27
Sara Silva <saracsas2@gmail.com>
2021-05-15 18:25:13 -0300

    Add wifi module

---
+++ client/main/src/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "freertos/semphr.h"
+
+#include "wifi.h"
+
+xSemaphoreHandle conexaoWifiSemaphore;
+
+void test(void * params){
+    while(true){
+        printf("Hello, word!\n");
+    }
+}
+
+void app_main(void)
+{
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    
+    conexaoWifiSemaphore = xSemaphoreCreateBinary();
+    wifi_start();
+
+    xTaskCreate(&test,  "Processa HTTP", 4096, NULL, 1, NULL);
+}
+
